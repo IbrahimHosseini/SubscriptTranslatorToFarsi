96c5a58c743366035f8c25c64e47c14e15a84c58
Ibrahim Hosseini <sehosseini@me.com>
2024-12-24 00:34:58 +0330

    add openai libs

--- SubscriptTranslator.py
+++ SubscriptTranslator.py
@@ -1,13 +1,21 @@
 import os
-from deep_translator import GoogleTranslator
-
-translator = GoogleTranslator(source='auto', target='fa')
+from deep_translator import (GoogleTranslator, ChatGptTranslator)
+import openai
 
 nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] 
 
 file_path = "../SubscriptTranslatorToFarsi"
 src_file_name = "src_sub.srt"
 dest_file_name = "dest_sub.srt"
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise RuntimeError("OPENAI_API_KEY environment variable is not set!")
+
+print(f"API key : {api_key}")
+
+google_translator = GoogleTranslator(source='auto', target='fa')
+gpt_translator = ChatGptTranslator(api_key=api_key, target='fa', model='gpt-3.5-turbo')
 
 # join file and path
 src_file = os.path.join(file_path, src_file_name)
@@ -25,5 +33,5 @@ with open(dest_file, "w", encoding="utf-8") as read_file:
         elif sub_line == "\n":
             read_file.write("\n\n")
         else:
-            line_en = translator.translate(sub_line)
+            line_en = gpt_translator.translate(sub_line)
             read_file.write(line_en)
